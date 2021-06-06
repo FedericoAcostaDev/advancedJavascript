efd286ce5d7a676e4dfc6df9256fa8f9dc5a808b
FedericoAcostaDev <fedeacos045@gmail.com>
2021-06-06 11:51:04 -0300

    exercise finished

--- 15.2 exercise6.js.js
+++ 15.2 exercise6.js.js
@@ -1,7 +1,9 @@
 //Evaluate these:
 //#1
-[2] === [2] 
-{} === {} 
+[2] === [2] //false 
+{} === {} //false
+
+//THey are false because they are objects
 
 //#2 what is the value of property a for each object.
 const object1 = { a: 5 }; 
@@ -13,3 +15,23 @@ object1.a = 4;
 
 //#3 create two classes: an Animal class and a Mamal class. 
 // create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+class Animal {
+    constructor(name,type,color) {
+        console.log(this);
+        this.name = name;
+        this.type = type;
+        this.color = color;
+    }
+    sound() {
+        console.log(`Hi I am ${this.name}, I'm a ${this.type}, i am ${this.color}`)
+    }
+    }
+    class Mamal extends Animal{
+        constructor(name,type,color){
+            super(name,type,color)
+        }
+    }
+
+    const cow = new Mamal('shelly','mamal','black and white')
+
+//
